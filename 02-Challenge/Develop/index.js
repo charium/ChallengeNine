fee8b9dd13d5169c968f47968fefe2fe98261b80
Chloe Hall <ha11@ad.unc.edu>
2024-05-31 17:44:43 -0400

    Fixes questions and corrosponding code.

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,24 +1,214 @@
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+
+// // TODO: Create an array of questions for user input
+// const questions = [
+//     "Please enter the title of your application?", 
+//     "Enter the description of your application?",
+//     "How to install this application?", 
+//     "How to use this application?",
+//     "What license does this program use?", 
+//     "Name the contributors' GitHub usernames separated by a comma",
+//     "How should tests be performed?", 
+//     "Preferred method of contact?", 
+//     "Message to what phone number?", 
+//     "Message to what email?", 
+//     "What GitHub username should be messaged?", 
+// ];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     if (fs.existsSync(fileName)) {
+//         fs.unlinkSync(fileName); 
+//     }
+
+//     fs.writeFile(fileName, data, (err) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("File written successfully");
+//         }
+//     });
+// }
+
+// function renderLicenseLink(license) {
+//     switch (license) {
+//         case 'MIT License':
+//             return `[MIT license](https://opensource.org/licenses/MIT)`;
+//         case 'GNU General Public License':
+//             return `[GNU General Public License](https://opensource.org/licenses/GPL-3.0)`;
+//         case `Apache License 2.0`:
+//             return `[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)`;
+//         case `BSD License`:
+//             return `[BSD License](https://opensource.org/licenses/BSD-3-Clause)`;
+//         case `Mozilla Public License 2.0`:
+//             return `[Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)`;
+//         case `Creative Commons License`:
+//             return `[Creative Commons License](https://creativecommons.org/licenses/)`;
+//         case `None`:
+//             return `None`;
+// }}
+
+// // Function to generate README content
+// function generateReadMe(response, contactLine) {
+//     const contributorsList = response.contributors.split(',')
+//         .map(contributor => contributor.trim())
+//         .map(contributor => `[${contributor}](https://github.com/${contributor})`)
+//         .join('\n');
+
+//     return `
+// # ${response.title}, ${renderLicenseLink(response.license)}
+// ${response.description}
+
+// ## Table of Contents
+// - [Description](#description)
+// - [Table of Contents](#table-of-contents)
+// - [Installation](#installation)
+// - [Usage](#usage)
+// - [License](#license)
+// - [Contributing](#contributing)
+// - [Tests](#tests)
+// - [Questions](#questions)
+
+// ## Installation
+// ${response.instructions}
+
+// ## Usage
+// ${response.usage}
+
+// ## Repository Link
+// [Repository Link](${response.linkRepo})
+
+// ## Deployed Page Link
+// [Deployed Page Link](${response.linkDeployed})
+
+// ## License
+// This application falls under the ${response.license}. Find further details in the repository.
+
+// ## Contributing
+// Contributing members are thanked and appreciated:
+// ${contributorsList}
+
+// ## Tests
+// ${response.tests}
+
+// ## Questions
+// If you have any questions, contact a team member by ${response.contact} at ${contactLine}`;
+// }
+
+
+// // TODO: Create a function to initialize app
+// function init() {
+//     inquirer
+//         .prompt([
+//             {
+//                 type: 'input',
+//                 message: questions[0],
+//                 name: 'title',
+//             },
+//             {
+//                 type: 'input',
+//                 message: questions[1],
+//                 name: 'description',
+//             },
+//             {
+//                 type: 'input',
+//                 message: questions[2],
+//                 name: 'instructions',
+//             },
+//             {
+//                 type: 'input',
+//                 message: questions[3],
+//                 name: 'usage',
+//             },
+//             {
+//                 type: 'list',
+//                 message: questions[4],
+//                 choices: ['MIT License','GNU General Public License','LicenseApache License 2.0','BSD License','Mozilla Public License 2.0','Creative Commons License'],
+//                 name: 'license',
+//             },
+//             {
+//                 type: 'input',
+//                 message: questions[5],
+//                 name: 'contributors',
+//             },
+//             {
+//                 type: 'input',
+//                 message: questions[6],
+//                 name: 'tests',
+//             },
+//             {
+//                 type: 'list',
+//                 message: questions[7],
+//                 choices: ['phone', 'email', 'github'],
+//                 name: 'contact',
+//             },
+//         ])
+//         .then((response) => {
+//             let contactLine = '';
+//             if (response.contact === 'phone') {
+//                 inquirer.prompt([
+//                     {
+//                         type: 'input',
+//                         message: questions[8],
+//                         name: 'phone',
+//                     }
+//                 ]).then((contactResponse) => {
+//                     contactLine = contactResponse.phone;
+//                     const readMe = generateReadMe(response, contactLine);
+//                     writeToFile('README.md', readMe);
+//                 });
+//             } else if (response.contact === 'email') {
+//                 inquirer.prompt([
+//                     {
+//                         type: 'input',
+//                         message: questions[9],
+//                         name: 'email',
+//                     }
+//                 ]).then((contactResponse) => {
+//                     contactLine = contactResponse.email;
+//                     const readMe = generateReadMe(response, contactLine);
+//                     writeToFile('README.md', readMe);
+//                 });
+//             } else if (response.contact === 'github') {
+//                 inquirer.prompt([
+//                     {
+//                         type: 'input',
+//                         message: questions[10],
+//                         name: 'github',
+//                     }
+//                 ]).then((contactResponse) => {
+//                     contactLine = contactResponse.github;
+//                     const readMe = generateReadMe(response, contactLine);
+//                     writeToFile('README.md', readMe);
+//                 });
+//             };
+//         });
+// }
+
+// // Function call to initialize app
+// init();
+
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
+// Questions array for user input
 const questions = [
     "Please enter the title of your application?", 
     "Enter the description of your application?",
     "How to install this application?", 
     "How to use this application?",
-    "Please enter the repo link", 
-    "Please enter the deployed page link", 
     "What license does this program use?", 
     "Name the contributors' GitHub usernames separated by a comma",
     "How should tests be performed?", 
     "Preferred method of contact?", 
-    "What phone number?", 
-    "What email?", 
-    "What GitHub should be messaged?"
+    "Message to what phone number?", 
+    "Message to what email?", 
+    "What GitHub username should be messaged?", 
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
         if (err) {
@@ -29,6 +219,25 @@ function writeToFile(fileName, data) {
     });
 }
 
+function renderLicenseLink(license) {
+    switch (license) {
+        case 'MIT License':
+            return `[MIT license](https://opensource.org/licenses/MIT)`;
+        case 'GNU General Public License':
+            return `[GNU General Public License](https://opensource.org/licenses/GPL-3.0)`;
+        case `Apache License 2.0`:
+            return `[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)`;
+        case `BSD License`:
+            return `[BSD License](https://opensource.org/licenses/BSD-3-Clause)`;
+        case `Mozilla Public License 2.0`:
+            return `[Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)`;
+        case `Creative Commons License`:
+            return `[Creative Commons License](https://creativecommons.org/licenses/)`;
+        case `None`:
+            return `None`;
+    }
+}
+
 // Function to generate README content
 function generateReadMe(response, contactLine) {
     const contributorsList = response.contributors.split(',')
@@ -37,7 +246,7 @@ function generateReadMe(response, contactLine) {
         .join('\n');
 
     return `
-# ${response.title}
+# ${response.title}, ${renderLicenseLink(response.license)}
 ${response.description}
 
 ## Table of Contents
@@ -56,14 +265,8 @@ ${response.instructions}
 ## Usage
 ${response.usage}
 
-## Repository Link
-[Repository Link](${response.linkRepo})
-
-## Deployed Page Link
-[Deployed Page Link](${response.linkDeployed})
-
 ## License
-Please refer to the ${response.license} in the repo.
+This application falls under the ${response.license}. Find further details in the repository.
 
 ## Contributing
 Contributing members are thanked and appreciated:
@@ -76,7 +279,7 @@ ${response.tests}
 If you have any questions, contact a team member by ${response.contact} at ${contactLine}`;
 }
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
     inquirer
         .prompt([
@@ -101,49 +304,41 @@ function init() {
                 name: 'usage',
             },
             {
-                type: 'input',
-                message: questions[4],
-                name: 'linkRepo',
-            },
-            {
-                type: 'input',
-                message: questions[5],
-                name: 'linkDeployed',
-            },
-            {
                 type: 'list',
-                message: questions[6],
-                choices: ['MIT License','GNU General Public','LicenseApache License 2.0','BSD License','Mozilla Public License 2.0','Creative Commons License'],
+                message: questions[4],
+                choices: ['MIT License','GNU General Public License','Apache License 2.0','BSD License','Mozilla Public License 2.0','Creative Commons License'],
                 name: 'license',
             },
             {
                 type: 'input',
-                message: questions[7],
+                message: questions[5],
                 name: 'contributors',
             },
             {
                 type: 'input',
-                message: questions[8],
+                message: questions[6],
                 name: 'tests',
             },
             {
                 type: 'list',
-                message: questions[9],
+                message: questions[7],
                 choices: ['phone', 'email', 'github'],
                 name: 'contact',
             },
         ])
         .then((response) => {
+            console.log('Primary Response:', response);
             let contactLine = '';
             if (response.contact === 'phone') {
                 inquirer.prompt([
                     {
                         type: 'input',
-                        message: questions[10],
+                        message: questions[8],
                         name: 'phone',
                     }
                 ]).then((contactResponse) => {
                     contactLine = contactResponse.phone;
+                    console.log('Contact Response (Phone):', contactResponse);
                     const readMe = generateReadMe(response, contactLine);
                     writeToFile('README.md', readMe);
                 });
@@ -151,11 +346,12 @@ function init() {
                 inquirer.prompt([
                     {
                         type: 'input',
-                        message: questions[11],
+                        message: questions[9],
                         name: 'email',
                     }
                 ]).then((contactResponse) => {
                     contactLine = contactResponse.email;
+                    console.log('Contact Response (Email):', contactResponse);
                     const readMe = generateReadMe(response, contactLine);
                     writeToFile('README.md', readMe);
                 });
@@ -163,11 +359,12 @@ function init() {
                 inquirer.prompt([
                     {
                         type: 'input',
-                        message: questions[12],
+                        message: questions[10],
                         name: 'github',
                     }
                 ]).then((contactResponse) => {
                     contactLine = contactResponse.github;
+                    console.log('Contact Response (GitHub):', contactResponse);
                     const readMe = generateReadMe(response, contactLine);
                     writeToFile('README.md', readMe);
                 });
